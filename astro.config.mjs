50596419f39ed20e8dfec2c9a010d57a0f16aef6
ndeyros <ndeyros@lenovo.com>
2023-11-29 12:46:06 -0300

    fix: add vercel deployment method

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import partytown from '@astrojs/partytown'
-import react from '@astrojs/react'
-import prefetch from '@astrojs/prefetch'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
+import react from '@astrojs/react';
+import prefetch from '@astrojs/prefetch';
+import node from '@astrojs/node';
 
-import node from '@astrojs/node'
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), partytown(), react(), prefetch()],
-	output: 'server',
-	adapter: node({
-		mode: 'standalone',
-	}),
-})
+  integrations: [tailwind(), partytown(), react(), prefetch()],
+  output: 'server',
+  adapter: vercel()
+});
